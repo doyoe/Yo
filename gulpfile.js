188ee599a6576f17041e6408051dcb48fe90324a
Joy Du <dooyoe@gmail.com>
2015-04-16 15:56:33 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,6 @@ var scssPath = './usage/demo/page';
 var cssPath = './usage/demo/export';
 
 // 编译Sass
-// 只能编译不超过16个文件，谁能破，请告诉我呀
 gulp.task('compass', function() {
   gulp.src(scssPath + '/*.scss')
     .pipe(compass({
@@ -18,6 +17,8 @@ gulp.task('compass', function() {
         sass: scssPath,
         style: 'compressed'
     }))
+    // 本行不能删除，否则只能编译不超过16个文件
+    .pipe(gulp.dest(cssPath));
 });
 
 // watch变更
